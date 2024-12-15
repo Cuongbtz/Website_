c7fb43906a28c33cf7e642b02ecff8531652ecd0
Beateraz <cuongza399@gmail.com>
2024-12-15 10:30:45 +0700

    Wesite bán máy tinh

---
+++ laptopweb/admincp/modules/menu.php
@@ -0,0 +1,10 @@
+<div class="sidebar">
+    <h4>Menu</h4>
+    <a href="index.php?action=quanlydanhmucsanpham&query=them">Quản lí danh mục</a>
+    <a href="index.php?action=quanlysanpham&query=them">Quản lý sản phẩm</a>
+    <a href="#">Quản lý đơn hàng</a>
+    <a href="#">Quản lý tài khoản</a>
+    <a href="#">Thống kê</a>
+
+
+</div>
